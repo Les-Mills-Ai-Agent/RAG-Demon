6b5ba5f5a0d3a96bff1d8d448f52f663f4f38858
jakonito10 <jankarlorn@gmail.com>
2025-05-15 13:24:25 +1200

    chore: clean indentation in web_scrape module

--- ragdemon/web_scrape.py
+++ ragdemon/web_scrape.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 from langchain_text_splitters import (
     RecursiveCharacterTextSplitter,
     MarkdownHeaderTextSplitter,
-    RecursiveJsonSplitter
+    RecursiveJsonSplitter,
 )
 
 # Load environment variables
@@ -17,60 +17,62 @@ os.getenv("USER_AGENT")
 
 
 def fetch_documentation(url):
-        loader = WebBaseLoader(url)
-        doc_list = loader.load()
-        
-        document_str = doc_list[0].page_content
-        parsed_doc = yaml.safe_load(document_str)
+    loader = WebBaseLoader(url)
+    doc_list = loader.load()
 
-        return [parsed_doc]
-    
-    
+    document_str = doc_list[0].page_content
+    parsed_doc = yaml.safe_load(document_str)
+
+    return [parsed_doc]
+
+
 def extract_markdown(obj):
-        md_sections = []
-        
-        if isinstance(obj, dict):
-            for key, value in obj.items():
-                if isinstance(value, str) and any(token in value for token in ['#', '*', '`']):
-                    md_sections.append(value)
-                elif isinstance(value, (dict, list)):
-                    md_sections.extend(extract_markdown(value))
-                    
-        elif isinstance(obj, list):
-            for item in obj:
-                md_sections.extend(extract_markdown(item))
-                
-        return md_sections
-    
+    md_sections = []
+
+    if isinstance(obj, dict):
+        for key, value in obj.items():
+            if isinstance(value, str) and any(
+                token in value for token in ["#", "*", "`"]
+            ):
+                md_sections.append(value)
+            elif isinstance(value, (dict, list)):
+                md_sections.extend(extract_markdown(value))
+
+    elif isinstance(obj, list):
+        for item in obj:
+            md_sections.extend(extract_markdown(item))
+
+    return md_sections
+
 
 def split_document(document) -> List[Document]:
 
-        # Create JSON splits
-        json_splitter = RecursiveJsonSplitter(max_chunk_size=1000)
-        json_docs = json_splitter.create_documents(document)
-        
-        # Retrieve nested Markdown snippets
-        markdown_strings = extract_markdown(document)
-        combined_markdown = "\n\n".join(markdown_strings)
+    # Create JSON splits
+    json_splitter = RecursiveJsonSplitter(max_chunk_size=1000)
+    json_docs = json_splitter.create_documents(document)
 
-        # Create Markdown splits
-        headers_to_split_on = [
-            ("#", "Header 1"),
-            ("##", "Header 2"),
-            ("###", "Header 3"),
-            ("####", "Header 4"),
-        ]
+    # Retrieve nested Markdown snippets
+    markdown_strings = extract_markdown(document)
+    combined_markdown = "\n\n".join(markdown_strings)
 
-        md_splitter = MarkdownHeaderTextSplitter(headers_to_split_on)
-        md_splits = md_splitter.split_text(combined_markdown)
+    # Create Markdown splits
+    headers_to_split_on = [
+        ("#", "Header 1"),
+        ("##", "Header 2"),
+        ("###", "Header 3"),
+        ("####", "Header 4"),
+    ]
 
-        text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size=1000,
-            chunk_overlap=50,
-            separators=[". ", "\n\n", "\n"],
-        )
+    md_splitter = MarkdownHeaderTextSplitter(headers_to_split_on)
+    md_splits = md_splitter.split_text(combined_markdown)
 
-        md_docs = text_splitter.split_documents(md_splits)
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=50,
+        separators=[". ", "\n\n", "\n"],
+    )
 
-        # Combine and return both JSON and Markdown splits
-        return json_docs + md_docs
+    md_docs = text_splitter.split_documents(md_splits)
+
+    # Combine and return both JSON and Markdown splits
+    return json_docs + md_docs
