e6c4482684c6b84736242edfa0f138a6925eb64d
StephenDelaCruz <stephendelacruzz@gmail.com>
2025-08-15 00:25:37 +1200

    Feature: Implemented the Bedrock Guardrail

--- backend/bedrock_impl/src/handler.py
+++ backend/bedrock_impl/src/handler.py
@@ -15,6 +15,10 @@ def get_bedrock_client() -> AgentsforBedrockRuntimeClient:
 def get_bedrock_config() -> RetrieveAndGenerateConfigurationTypeDef:
     return RetrieveAndGenerateConfigurationTypeDef(
         type="KNOWLEDGE_BASE",
+        guardrailConfiguration = {
+            "guardrailId": "3x3fwig8roag",
+            "guardrailVersion": "1"
+        },
         knowledgeBaseConfiguration={
             "knowledgeBaseId": "XBOBJWN1MQ",
             "modelArn": "anthropic.claude-3-5-sonnet-20240620-v1:0"
