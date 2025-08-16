954f4bbfcb0644f488b538c921792ef61a49fe53
jakonito10 <jankarlorn@gmail.com>
2025-08-17 11:00:16 +1200

    refactor: web scrap/splitting unit test

--- Backend/tests/test_web_scrape.py
+++ Backend/tests/test_web_scrape.py
@@ -2,7 +2,6 @@ import pytest
 from langchain_core.documents import Document
 from dotenv import load_dotenv
 from src.ragdemon.web_scrape import fetch_documentation
-from src.ragdemon.web_scrape import separate_markdown_from_yaml
 from src.ragdemon.web_scrape import split_document
 import yaml
 
@@ -11,33 +10,8 @@ load_dotenv(override=True)
 
 @pytest.fixture
 def test_data():
-    with open("Backend/sample_data/test_data.yaml", "r") as file:
+    with open("../sample_data/test_data.yaml", "r") as file:
         return yaml.safe_load(file)
-    
-@pytest.fixture
-def markdown_sections():
-    return [
-        "# Welcome to how to use Content Portal API\nContent 1\n\n## Hello I am Header 2\nContent 2", 
-        "# How to use videos API\nContent 1\n\n## Hello I am Header 2\nContent 2"
-    ]
-
-@pytest.fixture
-def cleaned_yaml():
-    return {
-        "openapi": "3.1.0",
-        "info": {
-            "version": "1.0",
-            "title": "Company Content Portal API",
-            "description": None
-        },
-        "paths": {
-            "items/Videos": {
-                "get": {
-                    "description": None
-                }
-            }
-        }
-    }
 
 def test_fetch_documentation():
     document = fetch_documentation("https://api.content.lesmills.com/docs/v1/content-portal-api.yaml")
@@ -45,25 +19,35 @@ def test_fetch_documentation():
     assert isinstance(document, dict)
     assert document['info']['title'] == "LesMills Content Portal API"
     
-def test_separate_markdown_from_yaml(test_data, markdown_sections, cleaned_yaml):
-    md, json = separate_markdown_from_yaml(test_data)
+def test_split_document(test_data):
+    splits = split_document(test_data)
     
-    assert md == markdown_sections
-    assert json == cleaned_yaml
-    
-    
-# Splitting to be improved further in new story
+    # First split (json_split)
+    assert splits[0].metadata["source"] == "json_split"
+    assert splits[0].page_content == '{"openapi": "3.1.0", "info.version": "1.0", "info.title": "Company Content Portal API"}'
 
-# def test_split_document(test_data):
-#     markdown_sections, cleaned_yaml = split_document(test_data)
-    
-#     assert len(markdown_sections) == 4
-#     assert markdown_sections[0].metadata["Header 1"] == "Welcome to how to use Content Portal API"
-#     assert markdown_sections[0].page_content == "Content 1"
-#     assert markdown_sections[1].metadata["Header 2"] == "Hello I am Header 2"
-#     assert markdown_sections[1].page_content == "Content 2"
-    
-#     assert markdown_sections[2].metadata["Header 1"] == "How to use videos API"
-#     assert markdown_sections[2].page_content == "Content 1"
-#     assert markdown_sections[3].metadata["Header 2"] == "Hello I am Header 2"
-#     assert markdown_sections[3].page_content == "Content 2"
+    # Second split (markdown_split - info.description, Header 1 only)
+    assert splits[1].metadata["source"] == "markdown_split"
+    assert splits[1].metadata["Header 1"] == "Welcome to how to use Content Portal API"
+    assert splits[1].metadata["key_path"] == "info.description"
+    assert splits[1].page_content == "Content 1"
+
+    # Third split (markdown_split - info.description, Header 1 + Header 2)
+    assert splits[2].metadata["source"] == "markdown_split"
+    assert splits[2].metadata["Header 1"] == "Welcome to how to use Content Portal API"
+    assert splits[2].metadata["Header 2"] == "Hello I am Header 2"
+    assert splits[2].metadata["key_path"] == "info.description"
+    assert splits[2].page_content == "Content 2"
+
+    # Fourth split (markdown_split - Videos API, Header 1 only)
+    assert splits[3].metadata["source"] == "markdown_split"
+    assert splits[3].metadata["Header 1"] == "How to use videos API"
+    assert splits[3].metadata["key_path"] == "paths.items/Videos.get.description"
+    assert splits[3].page_content == "Content 1"
+
+    # Fifth split (markdown_split - Videos API, Header 1 + Header 2)
+    assert splits[4].metadata["source"] == "markdown_split"
+    assert splits[4].metadata["Header 1"] == "How to use videos API"
+    assert splits[4].metadata["Header 2"] == "Hello I am Header 2"
+    assert splits[4].metadata["key_path"] == "paths.items/Videos.get.description"
+    assert splits[4].page_content == "Content 2"
